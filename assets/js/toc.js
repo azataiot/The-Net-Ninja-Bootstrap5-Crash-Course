86083fc64a9359409f87335cc79447e66ddf2051
Azat <8280770+azataiot@users.noreply.github.com>
2023-04-12 17:52:32 +0300

    updated the toc.js contents, so that now toc is working normally. fix #1

--- assets/js/toc.js
+++ assets/js/toc.js
@@ -1,19 +1,19 @@
 <!-- JavaScript to generate table of contents -->
 
 // Get the table of contents element
-var toc = document.getElementById('toc');
+const toc = document.getElementById('toc');
 
 // Find all h2, h3, and h4 elements in the document
-var headings = document.querySelectorAll('h2, h3, h4');
+const headings = document.querySelectorAll('h2, h3, h4');
 
 // Loop through the headings and add them to the table of contents
-for (var i = 0; i < headings.length; i++) {
-    var heading = headings[i];
+for (let i = 0; i < headings.length; i++) {
+    const heading = headings[i];
 
     // Create a link to the heading
-    var link = document.createElement('a');
+    const link = document.createElement('a');
     link.textContent = heading.textContent;
-    link.href = '#' + heading.id;
+    link.href = '#' + 'section-' + i;
 
     // Add the link to the table of contents
     toc.appendChild(link);
@@ -39,14 +39,14 @@ window.addEventListener('scroll', highlightActiveSection);
 
 function highlightActiveSection() {
     // Get the current scroll position
-    var scrollPosition = window.scrollY || window.pageYOffset;
+    const scrollPosition = window.scrollY || window.pageYOffset;
 
 // Find the current section by looking for the heading that is closest to
 // the current scroll position
-    var currentSection = null;
-    for (var i = 0; i < headings.length; i++) {
-        var heading = headings[i];
-        var headingPosition = heading.offsetTop;
+    let currentSection = null;
+    for (let i = 0; i < headings.length; i++) {
+        const heading = headings[i];
+        const headingPosition = heading.offsetTop;
 
         if (headingPosition <= scrollPosition) {
             currentSection = heading;
@@ -56,14 +56,14 @@ function highlightActiveSection() {
     }
 
     // Remove the "active" class from all links in the table of contents
-    var links = toc.getElementsByTagName('a');
-    for (var i = 0; i < links.length; i++) {
-        links[i].classList.remove('active');
+    const links = toc.getElementsByTagName('a');
+    for (let j = 0; j < links.length; j++) {
+        links[j].classList.remove('active');
     }
 
     // Add the "active" class to the link for the current section
     if (currentSection) {
-        var link = toc.querySelector('a[href="#' + currentSection.id + '"]');
+        const link = toc.querySelector('a[href="#' + currentSection.id + '"]');
         if (link) {
             link.classList.add('active');
         }
